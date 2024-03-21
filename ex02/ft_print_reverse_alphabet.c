a31058b6f7a3c48d66df93ac6e5c461a0fee1c0b
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-21 22:43:46 +0100

    Ejercicio 2

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 19:43:21 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/21 21:16:42 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+	
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write(1, &letra, 1);
+		letra--;
+	}
+}
+/*
+int main(){
+	ft_print_reverse_alphabet();
+	return 0;
+}
+*/
