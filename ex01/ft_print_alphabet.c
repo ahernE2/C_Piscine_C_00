3936fdf6fa9696a362399a50fd541e9a9aa0fd95
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-21 22:43:28 +0100

    Ejercicio 1

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 18:37:18 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/21 21:13:01 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letra;
+
+	letra = 'a';
+	while (letra <= 'z')
+	{
+		write(1, &letra, 1);
+		letra++;
+	}
+}
+
+/*
+int main(){
+	ft_print_alphabet();
+	return 0;
+}
+*/
