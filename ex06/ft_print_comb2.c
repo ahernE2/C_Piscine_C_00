e3bce8897351975850be9600e4f90394fb8b0fba
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-22 01:42:28 +0100

    Ejercicio 6

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 01:31:33 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/22 01:41:21 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	num1;
+	int	num2;
+
+	num1 = 0;
+	num2 = 0;
+	while (num1 <= 98)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 99)
+		{
+			ft_putchar((num1 / 10) + '0');
+			ft_putchar((num1 % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((num2 / 10) + '0');
+			ft_putchar((num2 % 10) + '0');
+			if (num1 != 98 || num2 != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+}
+/*
+int main() {
+    ft_print_comb2();
+    return 0;
+}
+*/
