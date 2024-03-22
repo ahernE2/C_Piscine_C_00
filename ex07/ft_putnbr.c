8a64e7723ab0fd23e7772c03380ac0fdf40a8aeb
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-22 01:46:29 +0100

    Ejercicio 7

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 00:54:22 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/22 01:44:59 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+/*
+int main() {
+    ft_putnbr(42);
+    return 0;
+}
+*/
