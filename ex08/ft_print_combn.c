9ecd32a563f5134269524da47d28ed9e29d56a56
Alejandro Hernandez Hernandez <alejhern@cbr12s5.42barcelona.com>
2024-03-23 19:41:54 +0100

    Porfin me lo he sacado

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 02:20:49 by alejhern          #+#    #+#             */
-/*   Updated: 2024/03/23 02:29:22 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/03/23 19:40:45 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,53 +16,51 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	imprimir(int numero[], int n)
+void	imprimir(int numero, int n)
 {
+	int	divisor;
 	int	i;
 
+	divisor = 1;
 	i = 0;
-	while (i < n) 
+	while (i < n - 1)
 	{
-		ft_putchar(numero[i] + '0');
+		divisor *= 10;
 		i++;
 	}
+	while (divisor > 0)
+	{
+		ft_putchar((numero / divisor) + '0');
+		numero %= divisor;
+		divisor /= 10;
+	}
 	ft_putchar(',');
-	ft_putchar(' ');	
+	ft_putchar(' ');
 }
 
 void	ft_print_combi(int n)
 {
-	int	numeros[n];
+	int	numero;
+	int	limite;
 	int	i;
 
+	numero = 0;
 	i = 0;
-	while (i < n)
+	limite = 1;
+	while (i < n -1)
 	{
-		numeros[i] = 0;
+		limite *= 10;
 		i++;
 	}
-	i = 0;
-	while (numeros[0] <= 9)
+	while (numero < limite)
 	{
-		imprimir(numeros, n);		
-		i = n - 1;
-		while (i >= 0 && numeros[i] == 9)
-		{
-			numeros[i] = 0;
-			i--;
-		}
-		if (i >= 0)
-		{
-			numeros[i]++;
-		}
-		else
-		{
-			break;
-        }
-    }
+		imprimir(numero, n);
+		numero++;
+	}
 }
 
-int main() {
-    ft_print_combi(3);
-    return 0;
+int	main(void)
+{
+	ft_print_combi(2);
+	return (0);
 }
