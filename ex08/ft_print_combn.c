a62b80ffac5389ec04785210c7632acd31b67759
Alejandro Hernandez Hernandez <alejhern@cbr9s5.42barcelona.com>
2024-03-24 19:19:08 +0100

    Falta quitar coma del final

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 02:20:49 by alejhern          #+#    #+#             */
-/*   Updated: 2024/03/23 19:40:45 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/03/24 19:17:47 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,6 +16,37 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int	tiene_digitos_repetidos(int numero)
+{
+	int	digitos[10];
+	int	digito;
+	int	c;
+
+	c = 0;
+	while (c < 9)
+	{
+		digitos[c] = '0';
+		c++;
+	}
+	while (numero > 0)
+	{
+		digito = numero % 10;
+		if (digitos[digito] == 1)
+			return (1);
+		digitos[digito] = 1;
+		numero /= 10;
+	}
+	return (0);
+}
+/*
+int is_last_value(int numero, int limite)
+{
+	if (tiene_digitos_repetidos(numero + 1) == 0)
+	{
+		return (0);
+	}	
+}
+*/
 void	imprimir(int numero, int n)
 {
 	int	divisor;
@@ -34,8 +65,6 @@ void	imprimir(int numero, int n)
 		numero %= divisor;
 		divisor /= 10;
 	}
-	ft_putchar(',');
-	ft_putchar(' ');
 }
 
 void	ft_print_combi(int n)
@@ -54,13 +83,18 @@ void	ft_print_combi(int n)
 	}
 	while (numero < limite)
 	{
-		imprimir(numero, n);
+		if (tiene_digitos_repetidos(numero) == 0)
+		{
+			imprimir(numero, n);
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
 		numero++;
 	}
 }
 
 int	main(void)
 {
-	ft_print_combi(2);
+	ft_print_combi(3);
 	return (0);
 }
