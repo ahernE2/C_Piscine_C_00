00141a1a67f7513cf062ccbb4fdddc896f5fe17f
Alejandro Hernandez Hernandez <alejhern@cbr9s5.42barcelona.com>
2024-03-25 00:13:42 +0100

    solo falta que se pongan las comas bien

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 02:20:49 by alejhern          #+#    #+#             */
-/*   Updated: 2024/03/24 19:17:47 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/03/24 23:12:34 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,85 +16,41 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	tiene_digitos_repetidos(int numero)
+void	print_recursive(int n, int start, int actual, int *combination)
 {
-	int	digitos[10];
-	int	digito;
-	int	c;
-
-	c = 0;
-	while (c < 9)
-	{
-		digitos[c] = '0';
-		c++;
-	}
-	while (numero > 0)
-	{
-		digito = numero % 10;
-		if (digitos[digito] == 1)
-			return (1);
-		digitos[digito] = 1;
-		numero /= 10;
-	}
-	return (0);
-}
-/*
-int is_last_value(int numero, int limite)
-{
-	if (tiene_digitos_repetidos(numero + 1) == 0)
-	{
-		return (0);
-	}	
-}
-*/
-void	imprimir(int numero, int n)
-{
-	int	divisor;
 	int	i;
+	int	d;
 
-	divisor = 1;
 	i = 0;
-	while (i < n - 1)
+	d = start;
+	if (actual== n)
 	{
-		divisor *= 10;
-		i++;
-	}
-	while (divisor > 0)
-	{
-		ft_putchar((numero / divisor) + '0');
-		numero %= divisor;
-		divisor /= 10;
-	}
-}
-
-void	ft_print_combi(int n)
-{
-	int	numero;
-	int	limite;
-	int	i;
-
-	numero = 0;
-	i = 0;
-	limite = 1;
-	while (i < n -1)
-	{
-		limite *= 10;
-		i++;
-	}
-	while (numero < limite)
-	{
-		if (tiene_digitos_repetidos(numero) == 0)
+		while (i < n)
 		{
-			imprimir(numero, n);
-			ft_putchar(',');
-			ft_putchar(' ');
+			ft_putchar(combination[i]);
+			i++;
 		}
-		numero++;
+		ft_putchar(',');
+		ft_putchar(' ');
+		return ;
 	}
+	while (d < 10)
+	{
+		combination[actual] = '0' + d;
+		print_recursive(n, d + 1, actual + 1, combination);
+		d++;
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	combination[10];
+
+	print_recursive(n, 0, 0, combination);
 }
 
 int	main(void)
 {
-	ft_print_combi(3);
+	ft_print_combn(4);
 	return (0);
 }
